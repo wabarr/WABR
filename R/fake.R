ef1ba10b70c806f5f224d2697886bd027e404499
wabarr <wabarr@gmail.com>
2014-04-30 15:51:39 -0500

    first fake function

---
+++ R/fake.R
@@ -0,0 +1,13 @@
+#' A Fake Function
+#'
+#' This function isn't real
+#' @param spoon_or_not whether or not there is a spoon. Default is FALSE
+#' @keywords spoons
+#’ @export
+#' @examples
+#' spoon()
+
+
+spoon <- function(spoon = FALSE) {
+  print("There Is No Spoon")
+}
