a1546da8ab73d8b97ced21a8765cc895076ee319
wabarr <wabarr@gmail.com>
2014-04-30 16:04:09 -0500

    munging

--- R/fake.R
+++ R/fake.R
@@ -4,10 +4,9 @@
 #' @param spoon_or_not whether or not there is a spoon. Default is FALSE
 #' @keywords spoons
 #’ @export
-#' @examples
-#' spoon()
+#' fake()
 
 
-spoon <- function(spoon = FALSE) {
+fake <- function(spoon = FALSE) {
   print("There Is No Spoon")
 }
