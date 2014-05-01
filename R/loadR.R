2c9de31825d7257879f447beb5dbe5c6f9dc600a
Andrew Barr <wabarr@gmail.com>
2014-05-01 15:03:57 -0500

    alster function so that quotation marks are not needed for package name

--- R/loadR.R
+++ R/loadR.R
@@ -1,16 +1,19 @@
 #' function to load package, or download packge if not currently installed
 #'
 #' This function loads a package with library() if installed, or downloads it from the Rstudio mirror if not
-#' @param package name of package as a character string
+#' @param package name of package (don't use quotation marks)
 #' @param mirror Default is Rstudio cloud mirror
 #' @keywords library require install package
 #' @export
 #' @examples
 #' loadR("devtools")
 
-loadR <- function(package, mirror = "http://cran.rstudio.com") {
-  stopifnot(is.character(package))
+loadR <- function (package, mirror = "http://cran.rstudio.com") 
+{
+  stringPackage <- deparse(substitute(package))
+  stopifnot(is.character(stringPackage))
   stopifnot(is.character(mirror))
-  if (!package %in% row.names(installed.packages())) install.packages(package, repos= c(CRAN = mirror), depend=TRUE)
-  library(package, character.only = TRUE)
+  if (!stringPackage %in% row.names(installed.packages())) 
+    install.packages(stringPackage, repos = c(CRAN = mirror), depend = TRUE)
+  library(stringPackage, character.only = TRUE)
 }
